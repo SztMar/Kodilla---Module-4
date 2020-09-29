aa91a54fd77f6e926d1daccabbf8bffcf81779ee
Marek <mareksztuczka1992@gmail.com>
2020-09-29 22:01:12 +0200

    Add Palindrome.py excercise

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+#Palindromy
+
+def palindrome(given_string):
+    string_without_space = given_string.replace(" ","").upper()
+    string_list = list(string_without_space)
+    string_reversed = string_list.copy()
+    string_reversed.reverse()
+    
+    if string_list == string_reversed:
+        print("True. Given sentence is a palindrome.")
+    else:
+        print("False. Given sentence is not palindrome!")
+    
+
+palindrome("Kajak")
